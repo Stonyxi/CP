ad0b123110695292a58a63cee3dcfe836e20e7ae
Stonyxi <126449424+Stonyxi@users.noreply.github.com>
2024-05-12 22:45:20 +0800

    Create N字形字串轉換.cpp

---
+++ leetcode/N字形字串轉換.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grip[1000][1000];
+
+string convert(string s, int numRows) {
+    int len = s.size(), x = 0;
+    vector<vector<char>> grip(numRows, vector<char>(len, ' ')); // 初始化 2D 向量，使用空格填充
+    int column = 0, row = numRows;
+    
+	if(numRows == 1) {
+	     return s;
+	}
+	
+    while (x < len) {
+        if( column % (row - 1) == 0) {
+            for (int i = 0; i < row && x < len; i++){
+                grip[i][column] = s[x++];
+            }
+            column++;
+        } 
+        else{
+            int up = row - 2;
+            while (x < len && up >= 1) {
+                grip[up--][column++] = s[x++];
+            }
+        }
+    }
+    string re;
+    for(auto i : grip) {
+	    for(auto j : i) {
+	     	if(j == ' ')continue;
+	     	re += j;   
+	    }
+		
+	}
+    return re;
+}
+
+int main(){
+	cout << convert("PAYPALISHIRING",3);
+	
+}
