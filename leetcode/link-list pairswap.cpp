f54398618cc9955cea52cbd51923a55a5336045e
Stonyxi <126449424+Stonyxi@users.noreply.github.com>
2024-05-14 09:29:10 +0800

    Create link-list pairswap.cpp

---
+++ leetcode/link-list pairswap.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+ListNode* swapPairs(ListNode* head) {
+	ListNode* fixvirhead = new ListNode();
+	ListNode* vir = fixhead;
+	ListNode* cur = head;
+	if(head->next==NULL)return head;
+	while(cur || cur->next){
+		vir->next = cur->next;
+		cur->next = vir ->next->next;
+		vir->next->next = cur;
+		
+		vir=cur;
+		cur=cur->next;
+	}        
+	return fixhead->next;
+}
+
+
